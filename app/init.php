35a3228e5657510d3351fcaf3d0d74f990efe797
Povilas <Povilas@Povilas-PC>
2014-10-21 11:43:03 +0300

    sutvarkytas init.php. pridetas autoload laravel

--- app/init.php
+++ app/init.php
@@ -1,4 +1,9 @@
 <?php
-
+//pajungiamas laravel modulis
+require_once '../vendor/autoload.php';
+//database pajungimas
+require_once 'database.php';
+//aplikacijos sukurimas
 require_once 'core/App.php';
+//pagrindinis kontroleris
 require_once 'core/Controller.php';
